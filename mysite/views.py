20787c9347523bfd5752fcdba5f32f8ad4271bc4
qx <1321131987@qq.com>
2018-03-30 20:53:47 +0800

    mysite

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render_to_response
-
+from read_statistics.utils import get_seven_days_read_nums
 def home(request):
-    return render_to_response('home.html')
+    read_num_list, date_list = get_seven_days_read_nums()
+    context = {}
+    context['read_num_list'] = read_num_list
+    context['date_list'] = date_list
+    return render_to_response('home.html', context=context)
     
