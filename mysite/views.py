fd464c2427d22f863cd233de0f8cee6e96f962d4
suploser <sup_loser@outlook.com>
2018-04-11 19:37:22 +0800

    view

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 from django.shortcuts import render,redirect
 from django.urls import reverse
+from django.http import JsonResponse
 from custom_user.models import User
 from blog.models import Blog
 from .forms import loginForm, RegForm
@@ -17,22 +18,48 @@ def home(request):
     context['date_list'] = date_list
     return render(request, 'home.html', context=context)
 
+def get_error(field, form):
+    return ','.join(form.errors.get(field,''))
 
-def login(request):
-    '''
-    username = request.POST.get('username', '')
-    password = request.POST.get('password', '')
-    user = User.objects.filter(username=username, password=password)
-    referer = request.META.get('HTTP_REFERER',reverse('home'))
-    if user:
+def overall_login(request):
+    data = {}
+    login_form = loginForm(request.POST)
+    if login_form.is_valid():
+        user = login_form.cleaned_data['user']
+        #写入session表的用户信息
         request.session.set_expiry(0)
-        request.session['username'] = username
-        request.session['password'] = password
+        request.session['username'] = user.username
+        request.session['password'] = user.password
         request.session['user'] = 'user'
-        return redirect(referer)
-    else:
-        return render(request, 'error.html', {'message':'用户名或密码错误'})
-        '''
+        data['status'] = 'Success'
+        data['referer'] = request.META.get('HTTP_REFERER', reverse('home'))
+        return JsonResponse(data)
+    data['status'] = 'Error'
+    data['username_error'] = get_error('username', login_form)
+    data['password_error'] = get_error('password', login_form)
+    data['non_field_error'] = ','.join(login_form.non_field_errors())
+    return JsonResponse(data)
+
+def overall_regist(request):
+    data = {}
+    reg_form = RegForm(request.POST)
+    if reg_form.is_valid():
+        username = reg_form.cleaned_data['username']
+        email = reg_form.cleaned_data['email']
+        password = reg_form.cleaned_data['password']
+        user = User(username=username, password=password, email=email)
+        user.save()
+        data['status'] = 'Success'
+        data['referer'] = request.META.get('HTTP_REFERER', reverse('home'))
+        return JsonResponse(data)
+    data['status'] = 'Error'
+    data['username_error'] = get_error('username', reg_form)
+    data['email_error'] = get_error('email',reg_form)
+    data['password_error'] = get_error('password', reg_form)
+    data['password_again_error'] = get_error('password_again', reg_form)
+    return JsonResponse(data)
+    
+def login(request): 
     login_form = loginForm()
     referer = request.GET.get('from', reverse('home'))
     if request.method == 'POST':
@@ -61,12 +88,10 @@ def regist(request):
             password = regist_form.cleaned_data['password']
             user = User(username=username, email=email, password=password)
             user.save()
-                #写入session表的    用户信息
-            request.session.set_expiry(0)
-            request.session['username'] = username
-            request.session['password'] = password
-            request.session['user'] = 'user'
+            #跳回登录页
             referer = request.GET.get('from', reverse('home'))
+            login_url = reverse('login')
+            referer = login_url+'?from='+referer
             return redirect(referer)
     return render(request, 'regist.html', {'regist_form':regist_form})
 
