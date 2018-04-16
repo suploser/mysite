f5e9ce517a2090d435960bfe76f0176293e7d68f
suploser <sup_loser@outlook.com>
2018-04-16 21:06:11 +0800

    admin

--- custom_user/admin.py
+++ custom_user/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import User
+from .models import User, ConfirmString
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'password', 'email')
+    list_display = ('username', 'password', 'email','has_confirmed')
+
+@admin.register(ConfirmString)
+class ConfirmStringAdmin(admin.ModelAdmin):
+    list_display = ('user', 'reg_time', 'token')
