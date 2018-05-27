683ce999b0e3ef9735d6a00d1fcf67fd96494826
suploser <sup_loser@outlook.com>
2018-05-27 20:18:43 +0800

    自定义标签

--- custom_user/templatetags/custom_user_tag.py
+++ custom_user/templatetags/custom_user_tag.py
@@ -1,7 +1,11 @@
 from django import template
-from custom_user.forms import ForgetPwdForm
+from custom_user.forms import ForgetPwdForm, ChangePwdForm
 
 register = template.Library()
 @register.simple_tag
 def get_pwd_form():
     return ForgetPwdForm()
+
+@register.simple_tag
+def get_change_pwd_form():
+    return ChangePwdForm()
