6b6c138ad5bdf69996da25906559aed3ce26b25b
suploser <sup_loser@outlook.com>
2018-06-13 14:05:41 +0800

    rm

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def send_confirm_email(email, token, referer='/'):
         感谢注册,但你的邮箱服务器不支持html链接功能,请联系管理员!
     '''
     html_content = '''
-        <p>感谢注册<a href="http://%s/confirm?token=%s&from=%s">个人博客</a></p>
+        <p>感谢注册<a href="http://%s/user/confirm?token=%s&from=%s">个人博客</a></p>
         <p>请点击链接完成注册!</p>
         <p>该链接的有效期为%s天</p>
     '''%('127.0.0.1:8000', token, referer, settings.CONFIRM_DAYS)
